af9b72a966a55055bcb7a6770703340de513847b
Tom Moyer <tom.moyer@uncc.edu>
2023-07-27 17:03:48 -0400

    More work on the underlying bits

--- src/projects.py
+++ src/projects.py
@@ -3,16 +3,19 @@
 # External libraries
 import typer
 import pprint
+import logging
 
 from typing import Optional
 from typing_extensions import Annotated
 from pathlib import Path
+from importlib.metadata import version
 
 # Internal files
 import notes
 import resources
 import times
 import containers
+import project
 from core_commands import *
 
 
@@ -22,17 +25,38 @@ app.add_typer(times.app, name='time', help='Manage time tracking for a project',
 app.add_typer(containers.app, name='containers', help='Manage containers for a project', rich_help_panel='Subcommands', callback=containers.project_callback)
 
 pp = pprint.PrettyPrinter()
-state = {'project': None}
+state = project.ProjectsState()
+logging.basicConfig(level=logging.WARNING,)
+_logger = logging.getLogger(__name__)
+
+
+def version_callback(value: bool):
+    if value:
+        typer.echo(f'Projects version: {version("projects")}')
+        raise typer.Exit()
 
 
 def first_run() -> None:
-    pass
+    '''
+    Does the initial creation stuff when things like config files and directories don't exist
+    This function should never make changes to existing files
+    '''
+    _logger.debug('Running first_run()')
+    state.config_directory.mkdir(mode=0o755, parents=True, exist_ok=True)
+    state.config_directory.joinpath('config.toml').touch(mode=0o644, exist_ok=True)
+    state.config_directory.joinpath('projects.state').touch(mode=0o644, exist_ok=True)
 
 
 @app.callback()
-def callback(config_directory: Annotated[Optional[Path], typer.Option(help='Directory to use for configuration files')] = Path('~/.config/projects/').expanduser()):
-    typer.echo('Running the callback function')
-    typer.echo(f'The config directory is: {config_directory}')
+def callback(config_directory: Annotated[Optional[Path], typer.Option(help='Directory to use for configuration files')] = Path('~/.config/projects/').expanduser(),
+             version: Annotated[Optional[bool], typer.Option("--version", callback=version_callback, is_eager=True, help='Print the version of projects')] = None,
+             debug: Annotated[Optional[bool], typer.Option("--debug", help='Enable debugging output')] = False,):
+    if debug:
+        _logger.setLevel(logging.DEBUG)
+        state.set_debug(debug)
+
+    state.config_directory = config_directory
+
     first_run()
 
 
